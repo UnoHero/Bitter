d22101244a4c5ba94f41f7cdf49c7322fb946cad
Scott <scottunogundersen@gmail.com>
2024-04-03 09:58:47 +0200

    connection to local CB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 
 // express app
 const app = express()
@@ -12,3 +13,15 @@ app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
