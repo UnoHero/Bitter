40620e76d541591d738c49077949bbd8c7d6b72d
Scott <scottunogundersen@gmail.com>
2024-04-03 10:14:04 +0200

    created routes for posts and users

---
+++ backend/routes/posts.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const {
+
+} = require('../controllers/postController')
+// const requireAuth = require('../middleware/requireAuth')
+
+const router = express.Router()
+
+// require auth
+//router.use(requireAuth)
+
+// GET all posts
+router.get("/", getPosts)
+
+// Get a single post
+router.get("/:id", getPost)
+
+// POST a new post
+router.post("/", createPost)
+
+// DELETE a workout
+router.delete("/:id", deletePost)
+
+// UPDATE a workout
+router.patch("/:id", updatePost)
+
+module.exports = router;
